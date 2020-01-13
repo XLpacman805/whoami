1bc77e6ced39b37b7473d635ba26c5afb1c9e603
Johnny Meza <webmanager@mxssolutions.com>
2020-01-12 21:22:21 -0800

    Added CORS support & changed response to json

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const addr = require("proxy-addr");
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://www.freecodecamp.org"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('views'));
 
@@ -16,18 +22,16 @@ app.use(express.static('views'));
 app.get("/api/whoami", (req, res) => {
   let agent = req.headers["user-agent"];
   let lang = req.headers["accept-language"];
-  let langEnd = lang.indexOf(","); //trims off excess data
-  lang = lang.slice(0, langEnd);
   
   let ip = addr(req, (trust) =>{
     return trust; 
   });
   
-  res.end(JSON.stringify(
+  res.json(
     {ipaddress: ip,
      language: lang,
      software: agent
-  }));
+  });
   
 });
 
